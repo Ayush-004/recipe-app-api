2f1f69e02e150a7387b53c7f23ecd2d4a3285318
Ayush-004 <ayush2003malik@gmail.com>
2024-08-18 20:53:47 +0530

    add userPage

--- app/core/admin.py
+++ app/core/admin.py
@@ -27,5 +27,19 @@ class UserAdmin(BaseUserAdmin):
         (_('Important dates'), {'fields':('last_login',)}),
     )
     readonly_fields=['last_login']
+    add_fieldsets=(
+        (None,{
+            'classes':('wide',), #makes fields look better in terms of wider
+            'fields':(
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 admin.site.register(models.User,UserAdmin) #tells to use custom UserAdmin class which we give,
