01a77e352942af84333228637b9880ecb7294cea
Ayush-004 <ayush2003malik@gmail.com>
2024-08-18 17:32:13 +0530

    add wait_for_db

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+'''
+Calculator functions
+'''
+
+def add(x,y):
+    '''
+    Add x and y and return result
+    '''
+    return x+y
+
+def substract(x,y):
+    '''
+    subtract y from x
+    '''
+    return x-y
